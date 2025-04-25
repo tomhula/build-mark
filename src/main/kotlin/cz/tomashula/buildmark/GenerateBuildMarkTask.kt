9d053e200418c6dc96c8ed65c6a9d54303c00e1a
Tomas Hula <tomashula06@gmail.com>
2025-04-25 21:25:35 +0200

    Handle hopefully all primitive types properly

--- src/main/kotlin/cz/tomashula/buildmark/GenerateBuildMarkTask.kt
+++ src/main/kotlin/cz/tomashula/buildmark/GenerateBuildMarkTask.kt
@@ -1,7 +1,6 @@
 package cz.tomashula.buildmark
 
 import com.squareup.kotlinpoet.FileSpec
-import com.squareup.kotlinpoet.KModifier
 import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeSpec
 import org.gradle.api.DefaultTask
@@ -9,57 +8,81 @@ import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import kotlin.reflect.KClass
-import kotlin.reflect.full.declaredMemberFunctions
-import kotlin.reflect.jvm.isAccessible
 
 internal abstract class GenerateBuildMarkTask : DefaultTask()
 {
     @get:OutputDirectory
     abstract val outputDir: DirectoryProperty
-    
+
     @TaskAction
     fun generate()
     {
         val buildMarkExt = project.extensions.getByType(BuildMarkExtension::class.java)
-        
-        val generatePropertyFunction = this::class.declaredMemberFunctions.first { it.name == "generateProperty" }
-        generatePropertyFunction.isAccessible = true
-        
+
         val properties = buildMarkExt.options.get().map { option ->
-            generatePropertyFunction.call(this, option.key, option.value::class, option.value) as PropertySpec
+            generateProperty(option.key, option.value::class, option.value.toString())
         }
-        
+
         val buildMarkObject = TypeSpec.objectBuilder(buildMarkExt.targetObjectName.get())
             .addProperties(properties)
             .build()
-        
+
         val file = FileSpec.builder(buildMarkExt.targetPackage.get(), buildMarkExt.targetObjectName.get())
             .addType(buildMarkObject)
             .build()
-        
+
         file.writeTo(outputDir.get().asFile)
     }
-    
-    private fun <T : Any> generateProperty(name: String, clazz: KClass<T>, value: T): PropertySpec
-    {
-        val isConst = primitiveTypes.any { clazz == it }
 
-        return PropertySpec.builder(name, clazz).apply { 
-            if (isConst)
-                addModifiers(KModifier.CONST)
-            when(clazz)
+    private fun <T : Any> generateProperty(name: String, clazz: KClass<T>, value: String): PropertySpec
+    {
+        val resultClass = clazz.takeIf { it in allPrimitiveTypes } ?: String::class
+
+        return PropertySpec.builder(name, resultClass).apply {
+            when (clazz)
             {
-                String::class -> initializer("%S", value)
                 Float::class -> initializer("%Lf", value)
-                else -> initializer("%L", value)
+                Char::class -> initializer("'%L'", value)
+                in unsignedTypes -> initializer("%Lu", value)
+                in directLiteralTypes -> initializer("%L", value)
+                else -> initializer("%S", value)
             }
-                
+
         }.build()
     }
-    
+
     companion object
     {
-        /** Primitive types, that support the `const` keyword. */
-        val primitiveTypes = listOf(String::class, Int::class, Long::class, Float::class, Double::class)
+        val allPrimitiveTypes = listOf<KClass<*>>(
+            Int::class,
+            Short::class,
+            Long::class,
+            Byte::class,
+            Float::class,
+            Double::class,
+            Boolean::class,
+            Char::class,
+            UInt::class,
+            UShort::class,
+            ULong::class,
+            UByte::class
+        )
+
+        /** All types, whose value as a string can be placed as literal as is. */
+        val directLiteralTypes = listOf<KClass<*>>(
+            Int::class,
+            Short::class,
+            Long::class,
+            Byte::class,
+            Double::class,
+            Boolean::class,
+        )
+
+        val unsignedTypes = listOf<KClass<*>>(
+            UInt::class,
+            UShort::class,
+            ULong::class,
+            UByte::class
+        )
     }
 }
