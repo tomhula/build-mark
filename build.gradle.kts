5ca2080376ad69d4a73568ae39119e36e8bc7b0e
Tomas Hula <tomashula06@gmail.com>
2025-04-29 12:57:05 +0200

    Add publishing to Gradle Plugin Portal

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
+    id("com.gradle.plugin-publish") version "1.3.1"
 }
 
 group = "cz.tomashula"
@@ -8,9 +9,14 @@ version = "1.0-SNAPSHOT"
 
 
 gradlePlugin {
+    website = "https://github.com/tomhula/build-mark"
+    vcsUrl = "https://github.com/tomhula/build-mark.git"
     plugins {
         create("buildMark") {
             id = "cz.tomashula.buildmark"
+            displayName = "BuildMark"
+            description = "Gradle plugin for embedding build information (like project name, version,...) into the code so it can be read at runtime."
+            tags = listOf("build", "version", "project", "kotlin")
             implementationClass = "cz.tomashula.buildmark.BuildMark"
         }
     }
